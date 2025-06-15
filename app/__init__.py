f094f07ba13724b0ec6751231ce505eff3453854
Douglas <douglas.ofc.2004@gmail.com>
2025-06-15 03:44:47 -0300

    chore: configura estrutura inicial do Flask (init.py e run.py)

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+# app/__init__.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_apscheduler import APScheduler
+from datetime import datetime
+
+# Inicialização das extensões
+db = SQLAlchemy()
+login_manager = LoginManager()
+scheduler = APScheduler()
+
+def create_app():
+    app = Flask(__name__, static_folder='static', template_folder='templates')
+    
+    # Configuração do app
+    app.config.from_mapping({
+        'SECRET_KEY': 'uma-chave-secreta-aqui',
+        'SQLALCHEMY_DATABASE_URI': 'sqlite://database.db',
+        'SCHEDULER_API_ENABLED': True
+    })
+
+    # Inicialização das extensões com o app
+    db.init_app(app)
+    login_manager.init_app(app)
+    scheduler.init_app(app)
+
+    # Importação dos blueprints
+    with app.app_context():
+        from app.routes.auth import bp as auth_bp
+        from app.routes.users import bp as users_bp
+        from app.routes.tracking import bp as contacts_bp
+        from app.routes.emergency import bp as emergency_bp
+
+        # Registro dos blueprints
+        app.register_blueprint(auth_bp, url_prefix='/auth')
+        app.register_blueprint(users_bp, url_prefix='/users')
+        app.register_blueprint(contacts_bp, url_prefix='/contacts')
+        app.register_blueprint(emergency_bp, url_prefix='/emergency')
+
+    return app
