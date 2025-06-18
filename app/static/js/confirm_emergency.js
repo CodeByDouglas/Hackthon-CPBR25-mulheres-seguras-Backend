b1855bf95bfb5cc26c99df4d4cd801f4ef984567
Douglas <douglas.ofc.2004@gmail.com>
2025-06-17 21:45:40 -0300

    refactor: separa CSS e JS do template confirm_emergency
    - Extrai estilos para arquivo CSS separado\n- Move scripts para arquivo JS separado\n- Atualiza template HTML para usar arquivos externos

---
+++ app/static/js/confirm_emergency.js
@@ -0,0 +1,9 @@
+function confirmEmergency() {
+    // Função será implementada posteriormente
+    console.log('Confirmar emergência');
+}
+
+function abortEmergency() {
+    // Função será implementada posteriormente
+    console.log('Abortar emergência');
+} 
